4ead095d34a64eac23093094a94a30c8dc9ffff3
Zuruuh <zuruh.dev@proton.me>
2025-01-20 22:33:36 +0000

    Create nix flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Mago is a toolchain for PHP that aims to provide a set of tools to help developers write better code.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, fenix }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      toolchain = fenix.packages.${system}.latest.toolchain;
+    in
+    {
+      packages.default = (pkgs.makeRustPlatform {
+        cargo = toolchain;
+        rustc = toolchain;
+      }).buildRustPackage {
+        pname = "mago";
+        version = "0.3.0";
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+
+        nativeBuildInputs = with pkgs; [ perl ];
+      };
+    });
+}
